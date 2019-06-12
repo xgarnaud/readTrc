1badb14d11afb717f55498fa21c09f73841a6ce1
Xavier GARNAUD <xavier.garnaud@airbus.com>
2019-06-12 14:32:04 +0200

    reorganized filed into a package

---
+++ read_trc/__init__.py
@@ -0,0 +1,1 @@
+from .trc import Trc
---
+++ setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name="read_trc",
+      version="0.1",
+      packages=['read_trc'],
+      install_requires=['numpy'],
+      )
